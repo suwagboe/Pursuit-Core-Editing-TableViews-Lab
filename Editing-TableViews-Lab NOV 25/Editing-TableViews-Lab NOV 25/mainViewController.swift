98c5c0fcbc1bbbc8f73a7a9cead67e44656ce8a2
shaniyaUwagboe <shaniyauwagboe@pursuit.org>
2019-11-25 16:37:29 -0500

    made the view controllers and the necessary items with contraints in each controller

---
+++ Editing-TableViews-Lab NOV 25/Editing-TableViews-Lab NOV 25/mainViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Editing-TableViews-Lab NOV 25
+//
+//  Created by World Domination a Brand on 11/25/19.
+//  Copyright © 2019 Pursuitful stuff. All rights reserved.
+//
+
+import UIKit
+
+class mainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
